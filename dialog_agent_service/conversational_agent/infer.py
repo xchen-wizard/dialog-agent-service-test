dbf4e00a843a95d45da578355a087c7b45be3dc4
Xi Chen <chen-xi@wizard.com>
2023-03-23 14:00:17 -0700

    unittested

--- dialog_agent_service/conversational_agent/infer.py
+++ dialog_agent_service/conversational_agent/infer.py
@@ -1,9 +1,11 @@
+from __future__ import annotations
+
 import glob
 import logging
-import traceback
 
 max_conversation_chars_task = 600
 max_conversation_chars_cart = 1500
+logger = logging.getLogger(__name__)
 
 
 class T5InferenceService:
@@ -18,18 +20,20 @@ class T5InferenceService:
             from transformers import T5Tokenizer, T5ForConditionalGeneration
             self.tokenizer = T5Tokenizer.from_pretrained(model_name)
             self.model = T5ForConditionalGeneration.from_pretrained(model_dir)
-        with open(f"{data_dir}/task_descriptions.txt") as f:
+        with open(f'{data_dir}/task_descriptions.txt') as f:
+            logger.info(f'loading task_descriptions.txt from {data_dir}')
             self.task_descriptions = f.read()
         self.response_prediction_prompt = dict()
-        fact_sheets = glob.glob(f"{data_dir}/*/fact_sheet.txt")
+        fact_sheets = glob.glob(f'{data_dir}/*/fact_sheet.txt')
         for fact_sheet in fact_sheets:
-            vendor = fact_sheet.split("/")[1]
+            vendor = fact_sheet.split('/')[2]
+            logger.info(f'loading fact sheets from {fact_sheet}')
             with open(fact_sheet) as f:
                 facts = f.read()
-            self.response_prediction_prompt[vendor] = f"{vendor} response: {facts}\n"
+            self.response_prediction_prompt[vendor] = f'{vendor} response: {facts}\n'
 
     def predict(self, text):
-        input = self.tokenizer(text, padding=True, return_tensors="pt")
+        input = self.tokenizer(text, padding=True, return_tensors='pt')
         outputs = self.model.generate(**input, max_new_tokens=128)
         return self.tokenizer.batch_decode(outputs, skip_special_tokens=True)
 
@@ -44,32 +48,40 @@ class T5InferenceService:
         :return: dict with different outputs of interest
         """
         # first predict task
-        input, _ = create_input_target_task(conversation, "", task_descriptions=self.task_descriptions)
+        input, _ = create_input_target_task(
+            conversation, '', task_descriptions=self.task_descriptions,
+        )
         task = predict_fn(input)[0]
-
+        logger.debug(f'predicted task {task}')
         if any(x in task for x in ['RecommendProduct', 'AnswerMiscellaneousQuestions']) and vendor in self.response_prediction_prompt:
-            conversation += "Seller: "
-            response = predict_fn(self.response_prediction_prompt[vendor] + conversation)[0]
-            return {"task": task, "response": response}
+            conversation += 'Seller: '
+            response = predict_fn(
+                self.response_prediction_prompt[vendor] + conversation,
+            )[0]
+            return {'task': task, 'response': response}
         if task == 'FinalizeOrder':
             return {'task': task}
         if task == 'CreateOrUpdateOrderCart':
-            product_input, _ = create_input_target_cart(conversation, "")
+            product_input, _ = create_input_target_cart(conversation, '')
             products = predict_fn(product_input)[0]
-            if products == "None":
-                return {"task": task, "cart": []}
+            if products == 'None':
+                return {'task': task, 'cart': []}
             else:
-                qty_input, _ = create_input_target_cart(conversation, products+";", qty_infer=True)
+                qty_input, _ = create_input_target_cart(
+                    conversation, products + ';', qty_infer=True,
+                )
                 qty_list = predict_fn(qty_input)
             try:
-                return {"task": task, "cart": list(zip(products.split(","), map(int, qty_list)))}
-            except:
-                logging.exception(traceback.format_exc())
-                return {"task": task}
+                return {'task': task, 'cart': list(zip(products.split(','), map(int, qty_list)))}
+            except Exception as e:
+                logger.error(
+                    f'encountered the following error while parsing products and quantities:\n{e}',
+                )
+                return {'task': task}
         return {'task': task}
 
 
-def create_input_target_task(cls, conversation, target_str, **kwargs):
+def create_input_target_task(conversation, target_str, **kwargs):
     return [f"""
 predict task based on task descriptions below:
 {kwargs['task_descriptions']}
@@ -79,25 +91,24 @@ Write a comma separated list of tasks that the buyer wants us to do right now.
 """], [target_str]
 
 
-def create_input_target_cart(cls, conversation, target_str, **kwargs):
+def create_input_target_cart(conversation, target_str, **kwargs):
     inputs = [f"""
-answer products of interest: 
+answer products of interest:
 {conversation[-max_conversation_chars_cart:]}
 Write a comma separated list of products that the buyer is interested in purchasing.
 """] if not kwargs.get('qty_infer', False) else []
-    products_qty = target_str.split(";")
+    products_qty = target_str.split(';')
     products = products_qty[0].strip()
     targets = [products]
     if len(products_qty) > 1:
-        targets.extend([s.strip() for s in products_qty[1].strip().split(",")])
-        products = products.split(",")
+        targets.extend([s.strip() for s in products_qty[1].strip().split(',')])
+        products = products.split(',')
         for product in products:
             inputs.append(
                 f"""
 answer product quantity:
 {conversation[-max_conversation_chars_cart:]}
 How many {product} does the buyer want?
-                """
+                """,
             )
     return inputs, targets
-
