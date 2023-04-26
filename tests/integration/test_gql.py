f650de3dfd67e74c24b7b76986f8cd4735a4ea0c
Xi Chen <chen-xi@wizard.com>
2023-04-26 16:02:02 -0700

    added gql connection and productLookUp

---
+++ tests/integration/test_gql.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+import logging
+import time
+
+import pytest
+
+from dialog_agent_service import get_gql_access_token
+from dialog_agent_service import init_gql
+from dialog_agent_service.db import product_search
+
+logger = logging.getLogger()
+
+
+def test_get_gql_access_token():
+    resp = get_gql_access_token()
+    assert resp is not None
+    logger.debug(resp.get('merchantUserLogin').get('accessToken'))
+    assert resp.get('merchantUserLogin').get('accessToken') is not None
+
+
+def test_init_gql(gql_client):
+    client = init_gql()
+    assert client is not None
+
+
+def test_product_search():
+    start = time.time()
+    resp = product_search('6', 'ancient dawn')
+    end = time.time()
+    logger.info(resp)
+    logger.info(end - start)
