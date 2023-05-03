b3a072c4b0aa6d94610a64006e7fe1e047bfaf02
Abhay Jha <abhay.jha@wizard.com>
2023-05-02 20:50:50 -0700

    Adding some extra checks for common cases

--- dialog_agent_service/conversational_agent/response.py
+++ dialog_agent_service/conversational_agent/response.py
@@ -4,17 +4,23 @@ newline = '\n'
 
 
 def gen_non_specific_product_response(product, matches):
-    return f"""
-    We found multiple items that match your search for {product}. Did you mean
-    {newline.join(f'{c}. {match}' for c, match in zip(string.ascii_uppercase, matches))}
-    or let us know if it is something else. 
-    """
+    if matches:
+        return f"""
+        We found multiple items that match your search for {product}. Did you mean
+        {newline.join(f'{c}. {match}' for c, match in zip(string.ascii_uppercase, matches))}
+        or let us know if it is something else. 
+        """
+    else:
+        # TODO: eventually we will just change the task to recommendation and let chatgpt respond
+        return f"""
+        We couldn't narrow down your search for {product}. Would you like some recommendations.
+        """
 
 
 def gen_variant_selection_response(product, variants):
     return f"""
     {product} is available as
-    {newline.join(f'{c}. {variant} (${price})' for c, (variant, price) in zip(string.ascii_uppercase, variants.items()))} 
+    {newline.join(f'{c}. {variant} (${price})' for c, (variant, price) in zip(string.ascii_uppercase, variants.items()))}
     Which one did you want?
     """
 
@@ -24,3 +30,9 @@ def gen_cart_response(cart):
     Your current cart has:
     {newline.join([f'- {name}  ${price} X {qty}' for name, price, qty in cart])}
     """ if cart else ""
+
+
+def gen_opening_response():
+    return """
+    Are there any particular products you are interested in? We can also help you select one if you are unsure. Let us know
+    """
