8db2c5417442367d1aa6831c5cd560b9d8c37444
Devang Kothari <ddkwiz@Devangs-Work-MPB.local>
2023-05-05 16:28:56 -0500

    removed duplicate handoff code

--- dialog_agent_service/conversational_agent/chatgpt.py
+++ dialog_agent_service/conversational_agent/chatgpt.py
@@ -1,4 +1,5 @@
 import os
+import re
 from textwrap import dedent
 import logging
 import openai
@@ -6,6 +7,7 @@ from .conversation_parser import Conversation, Turn
 
 MODEL = "gpt-3.5-turbo"
 TEMPERATURE = 0.2
+HANDOFF_TO_CX = 'HANDOFF TO CX|OpenAI|AI language model'
 
 logger = logging.getLogger(__name__)
 openai.api_key = os.getenv('OPENAI_API_KEY')
@@ -28,14 +30,17 @@ def conv_to_chatgpt_format(cnv_obj: Conversation, k):
 
 
 def answer_with_prompt(cnv_obj: Conversation, prompt):
-    response = openai.ChatCompletion.create(
+    prompt += '\nSeller: '
+    logger.debug(f"PROMPT:{prompt}")
+    resp = openai.ChatCompletion.create(
         model=MODEL,
         temperature=TEMPERATURE,
         messages=[
             {"role": "system", "content": prompt}
         ] + conv_to_chatgpt_format(cnv_obj, 5)
     )
-    return response.choices[0].message.content
+    llm_response = resp.choices[0].message.content
+    return validate_response(llm_response)
 
 
 def product_qa(cnv_obj: Conversation, data: str, vendor: str):
@@ -46,7 +51,6 @@ def product_qa(cnv_obj: Conversation, data: str, vendor: str):
     Limit responses to no more than 50 words.
     Product Data: {data}
     """).strip('\n')
-    logger.debug(f"PROMPT:{prompt}")
     return answer_with_prompt(cnv_obj, prompt)
 
 
@@ -59,11 +63,10 @@ def merchant_qa(cnv_obj: Conversation, data: str, vendor: str):
     Use the provided Instruction sections to further refine your response.
     Context: {data}
     Instructions:
-- if we are responding to an inbound for the first time, start with a greeting like "Hi there!" or "Thanks for your question!" before answering the question. Otherwise if we're in the middle of a conversation answer the question directly.
-- if the customer is asking about how to get free shipping, when we answer their question about free shipping thresholds, we should also check if there are other ongoing promotions and let them about any that exist (like a first text order discount).
-- unless the customer specifies otherwise, we should assume they are asking about shipping to the USA
+    - if we are responding to an inbound for the first time, start with a greeting like "Hi there!" or "Thanks for your question!" before answering the question. Otherwise if we're in the middle of a conversation answer the question directly.
+    - if the customer is asking about how to get free shipping, when we answer their question about free shipping thresholds, we should also check if there are other ongoing promotions and let them about any that exist (like a first text order discount).
+    - unless the customer specifies otherwise, we should assume they are asking about shipping to the USA
     """).strip('\n')
-    logger.debug(f"PROMPT:{prompt}")
     return answer_with_prompt(cnv_obj, prompt)
 
 
@@ -75,5 +78,15 @@ def recommend(cnv_obj: Conversation, data: str, vendor: str):
     Limit responses to no more than 50 words. 
     Context: {data}
     """).strip('\n')
-    logger.debug(f"PROMPT:{prompt}")
     return answer_with_prompt(cnv_obj, prompt)
+
+def validate_response(llm_response):
+    resp = {}
+    if re.search(HANDOFF_TO_CX, llm_response):
+        logger.info("LLM Failed Validation: handing off to CX")
+        resp = {'handoff': True, 'response': None}
+    else:
+        logger.info(f"LLM Response: {llm_response}")
+        resp = {'handoff': False, 'response': llm_response}
+
+    return resp
