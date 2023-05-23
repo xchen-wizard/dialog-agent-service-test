fdac828c8969f3ff8ad79791ae9eae66b32bacd5
abhay-kj <113057465+abhay-kj@users.noreply.github.com>
2023-05-23 08:27:06 -0700

    Revert "Update cart prediction" (#150)

--- dialog_agent_service/conversational_agent/task_handlers/task_handler.py
+++ dialog_agent_service/conversational_agent/task_handlers/task_handler.py
@@ -14,8 +14,4 @@ def camel_to_snake(name):
 
 def task_handler(task: str, **kwargs):
     handler = getattr(th, f"handle_{camel_to_snake(task)}", th.default_handler)
-    try:
-        return handler(**kwargs)
-    except Exception as e:
-        return th.default_handler(msg=f"Exception: {e}")
-
+    return handler(**kwargs)
