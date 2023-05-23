6fc4a679a10a6d767f1287c5b9b3e57f301dae62
Abhay Jha <abhay.jha@wizard.com>
2023-05-23 16:23:30 -0700

    Reverting to gpt3.5/fuzzywuzzy for cart

--- dialog_agent_service/conversational_agent/chatgpt.py
+++ dialog_agent_service/conversational_agent/chatgpt.py
@@ -8,7 +8,7 @@ import openai
 from .conversation_parser import Conversation, Turn
 from dialog_agent_service.constants import OpenAIModel
 
-TEMPERATURE = 0.2
+TEMPERATURE = 0.0 #for the tests
 HANDOFF_TO_CX = 'HANDOFF TO CX|OpenAI|language model'
 
 logger = logging.getLogger(__name__)
@@ -46,58 +46,44 @@ def answer_with_prompt(cnv_obj: Conversation, prompt, model=OpenAIModel.GPT35, t
     return validate_response(model, llm_response)
 
 
-def generate_cart_mentions(cnv_obj: Conversation, current_cart: List, products: List):
+def generate_cart_mentions(cnv_obj: Conversation, current_cart: List):
     context = "\n".join([str(t) for t in cnv_obj.turns[:-2]]) if cnv_obj.n_turns > 2 else ""
     last_seller_utt = str(cnv_obj.turns[-2]) if cnv_obj.n_turns > 1 else ""
-    current_cart_json = json.dumps([{"product": t[0], "quantity": t[1]} for t in current_cart])
+
     prompt = f"""
 BEGIN EXAMPLES
-List of products: ["Wizard Shampoo - 8 oz", "Wizard Shampoo - 16 oz", "Wizard Conditioner - 8 oz", "Wizard Conditioner - 16 oz", "Vanilla Ice Cream - 12 pk"]
-Buyer: how much does wizard shampoo cost?
-Cart: []
+Buyer: how does wizard shampoo cost?
+Buyer's Cart: []
 Seller: $12
 Buyer: Can you add wizard conditioner to my cart?
-Cart: [{{"product_mention": "wizard shampoo", "product": "Wizard Conditioner - 8 oz || Wizard Conditioner - 16 oz", "quantity": 1}}].
-Seller: 8 oz or 16 oz?
-Buyer: 8 oz
-Cart: [{{"product_mention": "wizard shampoo 8 oz", "product": "Wizard Conditioner - 8 oz", "quantity": 1}}]
+Buyer's Cart: [("wizard conditioner", 1)]
 Seller: Done! Anything else?
-Buyer: Add two
-Cart: [{{"product_mention": "wizard shampoo 8 oz", "product": "Wizard Conditioner - 8 oz", "quantity": 2}}]
-Seller: Of course! would you like to checkout now?
-Buyer: two of the ice cream?
-Cart: [{{"product_mention": "wizard shampoo 8 oz", "product": "Wizard Conditioner - 8 oz", "quantity": 2}}, {{"product_mention": "ice cream", "product": "Vanilla Ice Cream - 12 pk", "quantity": 2}}].
+Buyer: Also add two gummy bears?
+Buyer's Cart: [("wizard conditioner", 1), ("gummy bears", 2)]
 Seller: Sure. Are you ready to checkout now?
 Buyer: Actually make that one. and remove the conditioner.
-Cart: [{{"product_mention": "ice cream", "product": "Vanilal Ice Cream - 12 pk", "quantity": 1}}]
-Seller: done. Your cart has been updated.
-Buyer: i will get the shampoo
-Cart: [{{"product_mention": "ice cream", "product": "Vanilla Ice Cream - 12 pk", "quantity": 1}}, {{"product_mention": "shampoo", "product": "Wizard Shampoo - 8 oz || Wizard Shampoo - 16 oz", "quantity": 1}}].
+Buyer's Cart: [("gummy bears", 1)] 
 END EXAMPLES
-A Cart consists of the products that the buyer wants to purchase. To create the cart go through the conversation below
- and create a list of dictionaries with keys "product_mention", "product" and "quantity" where:
- - product_mention is the phrase used by the buyer to refer to the product. This may not be same as the product name
- - product is the name of the product that the buyer has asked to buy which has to come from the list provided below.The buyer does not always provide the exact product name, so you have to infer the product name from the list below based on their utterance. If there are multiple products from the list that are equally likely to be a match, you can output them all separated by delimiter "||".
- - quantity is an integer.
-Here is the list of all possible products: {products}
-BEGIN CONVERSATION
-{context}
-Cart: {current_cart_json}
-{last_seller_utt}
-{cnv_obj.turns[-1]}
-Cart:"""
+A buyer's cart consists of the products that they want to purchase. To create a buyer's cart go through the conversation above
+ and create a list of tuples of product X quantity, e.g. [("product1", quantity1), ("product2", quantity2), ...] where products are the products that the buyer has asked to buy or add to their cart and quantity is an integer.
+    {context}
+    Buyer's Cart: {current_cart}
+    {last_seller_utt}
+    {cnv_obj.turns[-1]}
+    Buyer's Cart: 
+    """
     messages = [
         {"role": "user", "content": prompt}
     ]
     resp = openai.ChatCompletion.create(
-        model=OpenAIModel.GPT4,
+        model=OpenAIModel.GPT35,
+        temperature=TEMPERATURE,
         messages=messages
     )
     llm_response = resp.choices[0].message.content
     st = llm_response.find('[')
     en = llm_response.find(']', st)
-    cart_json = ast.literal_eval(llm_response[st:en+1])
-    return cart_json
+    return ast.literal_eval(llm_response[st:en+1])
 
 
 def validate_response(model, llm_response):
--- dialog_agent_service/conversational_agent/resolve_cart.py
+++ dialog_agent_service/conversational_agent/resolve_cart.py
@@ -11,7 +11,6 @@ from dialog_agent_service.conversational_agent.response import gen_non_specific_
 from dialog_agent_service.conversational_agent.response import gen_variant_selection_response
 from dialog_agent_service.conversational_agent.response import gen_opening_response
 from dialog_agent_service import constants
-from typing import List
 
 ProductResponseUnion = namedtuple(
     'ProductResponseUnion', ['products', 'response'],
@@ -26,52 +25,53 @@ with open(f'{constants.ROOT_DIR}/test_data/products_variants_prices.json') as f:
     VARIANTS_OBJ = json.load(f)
     logger.info('loaded product variants and prices!')
 
-product_price_map = {
-    merchant_id: {(product + ' - ' + variant): price
-                  for product, d1 in d.items() for variant, price in d1.items()}
-    for merchant_id, d in VARIANTS_OBJ.items()
-}
-
 
 class MatchType(Enum):
     EXACT = 1
     DISAMBIGUATE = 2
 
 
-def match_mentions_to_products(merchant_id, mentions: List):
-    return [tup[0]
-            for mention in mentions
-            for tup in
-            process.extract(mention, product_price_map[merchant_id].keys(), scorer=fuzz.token_set_ratio, limit=10)]
-
-
-def get_product_price(merchant_id, product_name):
-    product_match, _ = process.extract(product_name, product_price_map[merchant_id].keys(), scorer=fuzz.token_set_ratio, limit=1)[0]
-    return product_price_map[merchant_id][product_match]
-
-
-def get_matches(query, string_list, sim_fn, threshold):
+def get_matches(query, string_list, sim_fn, threshold, prefer_exact=False):
     matches = process.extract(query, string_list, scorer=sim_fn)
     logger.info(f"query: {query}, matches: {matches}")
     exact_matches = [tup[0] for tup in matches if tup[1] == 100]
     if exact_matches and len(exact_matches) == 1:
-        return exact_matches, [tup[0] for tup in matches[:3]]
+        return exact_matches
     return [
-               tup[0]
-               for tup in matches
-               if tup[1] > threshold], [tup[0] for tup in matches[:3]]
+        tup[0]
+        for tup in matches
+        if tup[1] > threshold]
 
 
 def custom_match(query, string_list):
-    matches, all_matches = get_matches(query, string_list, fuzz.token_set_ratio, FUZZY_MATCH_THRESHOLD)
-    if not matches:
-        return MatchType.DISAMBIGUATE, all_matches[:4], all_matches
-    return MatchType.DISAMBIGUATE if len(matches) != 1 else MatchType.EXACT, matches, all_matches
+    matches = get_matches(query, string_list, fuzz.token_set_ratio, FUZZY_MATCH_THRESHOLD)
+    return MatchType.DISAMBIGUATE if len(matches) != 1 else MatchType.EXACT, matches
 
 
-def gen_disambiguation_response(merchant_id: str, product_name: str) -> str:
+def resolve_cart(merchant_id: str, cart: list[tuple[str, int]]):
+    if not cart:
+        return [], gen_opening_response()
+    resolved_cart = []
+    response = ""
+    for product, qty in cart:
+        products, product_response = match_product_variant(
+            merchant_id, product,
+        )
+        if products:
+            resolved_cart.extend([(p[0], p[1], qty) for p in products])
+        if product_response:
+            response += '\n' + product_response
+    # TODO: Cart summary will be enabled only after backend integration
+    if not response:
+        response = gen_cart_response(
+            resolved_cart,
+        ) + '\n' + response
+    return [(name, qty) for (name, _, qty) in resolved_cart], response
+
+
+def match_product_variant(merchant_id: str, product_name: str) -> ProductResponseUnion:
     merchant_id = str(merchant_id)
-    match_type, matches, all_matches = custom_match(product_name, VARIANTS_OBJ[merchant_id].keys())
+    match_type, matches = custom_match(product_name, VARIANTS_OBJ[merchant_id].keys())
     if match_type == MatchType.DISAMBIGUATE:
         """
         No matches with a high enough confidence. We disambiguate.
@@ -79,20 +79,40 @@ def gen_disambiguation_response(merchant_id: str, product_name: str) -> str:
         logger.debug(
             f'{product_name} search did not return a confident match',
         )
-        return gen_non_specific_product_response(
-            product_name, matches,
+        return ProductResponseUnion(
+            None, gen_non_specific_product_response(
+                product_name, matches,
+            ),
         )
     else:
         """
         Confident matches
         """
         products = []
+        response = ''
         for product_match in matches:
             variants_dict = VARIANTS_OBJ[merchant_id][product_match]
             if len(variants_dict) == 1:
-                # We assume that if the match was exact, LLM should have found it. So, we should not get to this part.'''
-                return gen_non_specific_product_response(product_name, all_matches)
-            else:
-                return gen_variant_selection_response(
-                    product_match, variants_dict
+                products.append(
+                    (product_match + ' - ' + min(variants_dict.keys()), min(variants_dict.values()))
                 )
+            else:
+                variant_matches = get_matches(
+                    product_name,
+                    [product_match + ' - ' + variant for variant in variants_dict.keys()],
+                    fuzz.token_set_ratio,
+                    FUZZY_MATCH_THRESHOLD,
+                    prefer_exact=True
+                )
+                if variant_matches and len(variant_matches) == 1:
+                    full_name = variant_matches[0]
+                    variant_name = full_name.split(" - ")[
+                        -1].strip()  # TODO: This is potentially problematic if the variant name has hyphen in it
+                    products.append((full_name, variants_dict[variant_name]))
+                else:
+                    response += '\n' + gen_variant_selection_response(
+                        product_match, variants_dict,
+                    )
+
+        return ProductResponseUnion(products, response)
+
