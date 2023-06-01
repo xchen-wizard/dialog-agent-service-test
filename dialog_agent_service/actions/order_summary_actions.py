2d1a74623429726b22f82efeab92d1ee1c461d75
Preston Rasmussen <prasmussen@Prestons-MacBook-Pro.local>
2023-06-01 18:06:05 -0400

    add order summary actions

---
+++ dialog_agent_service/actions/order_summary_actions.py
@@ -0,0 +1,36 @@
+import logging
+from gql import gql
+from dialog_agent_service import init_gql
+
+logger = logging.getLogger(__name__)
+gql_client = init_gql()
+
+def create_order_summary_message(cart_id: int):
+    query_str = gql('''
+        mutation CreateOrderSummaryMessage($cartId: Int!) {
+          createOrderSummaryMessage(cartId: $cartId) {
+            content {
+              body
+            }
+          }
+        }
+    ''')
+
+    vars = {
+        'cartId': cart_id
+    }
+
+    try:
+        resp = gql_client.execute(document=query_str, variable_values=vars)
+    except Exception as err:
+        logger.error(f'Create order summary message GQL-API request failed: {err}')
+        return None
+    
+    order_summary = resp['createOrderSummaryMessage']
+    if not order_summary:
+        logger.warn(f'createOrderSummaryMessage failed, no results merchant_id:{cart_id}')
+        return None
+    
+    logger.info(f'merchant_id:{cart_id}, createOrderSummaryMessage results: {order_summary}')
+
+    return order_summary
