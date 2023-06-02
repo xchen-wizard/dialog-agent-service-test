aca34f42d42736189409cc94e87e51931b2927b6
Preston Rasmussen <prasmussen@Prestons-MacBook-Pro.local>
2023-06-02 13:56:43 -0400

    create order summary message

--- dialog_agent_service/conversational_agent/task_handlers/handle_finalize_order.py
+++ dialog_agent_service/conversational_agent/task_handlers/handle_finalize_order.py
@@ -6,6 +6,7 @@ logger = logging.getLogger(__name__)
 
 def handle_finalize_order(cnv_obj=None, merchant_id=None, current_cart=None, predict_fn=None, **kwargs):
     message = create_order_summary_message(current_cart['id'])
+    logger.debug('order summary message:', message)
     return {
         'response': message,
         'vendorId': int(merchant_id),
