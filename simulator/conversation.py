6a9ad7c8d736b5b9bf974e47094b0c4dc9e989e5
Xi Chen <chen-xi@wizard.com>
2023-06-08 15:09:00 -0700

    updated

--- simulator/conversation.py
+++ simulator/conversation.py
@@ -1,7 +1,11 @@
 from __future__ import annotations
 
+import logging
 from datetime import datetime
 from typing import List
+
+
+logger = logging.getLogger()
 
 
 class Turn:
@@ -10,14 +14,24 @@ class Turn:
     def __init__(self, rows: list):
         self.direction = rows[0].direction
         self.text = [(row.createdAt, row.body) for row in rows if row.body]
+        self.session = [row.session for row in rows if row.session]
 
     def __str__(self):
-        header = 'Seller' if self.direction == 'outbound' else 'User'
+        header = 'Wizard' if self.direction == 'outbound' else 'User'
         return ': '.join([header, self.formatted_text])
 
     @property
     def formatted_text(self):
         return '\n'.join([t[1] for t in self.text])
+
+    def get_session(self):
+        if self.session:
+            if len(self.session) > 1:
+                logger.warning(
+                    f'got more than 1 session states for a turn: {self.session}')
+            return self.session[0]  # either start or end
+        else:
+            return None
 
     def test_format(self, threshold_in_seconds=300):
         return ' '.join([t[1] for t in self.text if (self.text[-1][0] - t[0]).seconds < threshold_in_seconds])
