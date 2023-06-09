9e00498a60c049d149e04b1e98d0e82a46987552
Xi Chen <chen-xi@wizard.com>
2023-06-09 11:47:13 -0700

    auto respond to clear_history
    auto respond to clear_history

--- dialog_agent_service/constants.py
+++ dialog_agent_service/constants.py
@@ -8,3 +8,7 @@ ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
 class OpenAIModel(str, Enum):
     GPT4 = "gpt-4"
     GPT35 = "gpt-3.5-turbo"
+
+
+HISTORY_CLEARED = 'HISTORY_CLEARED'
+CLEAR_HISTORY = 'CLEAR_HISTORY'
