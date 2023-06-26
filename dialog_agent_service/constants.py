d78dc46f10167ea7d04eaec30e97b6ac3075a546
abhay-kj <113057465+abhay-kj@users.noreply.github.com>
2023-06-26 13:19:06 -0700

    Keeping the same model version for cart (#251)

--- dialog_agent_service/constants.py
+++ dialog_agent_service/constants.py
@@ -8,6 +8,7 @@ ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
 class OpenAIModel(str, Enum):
     GPT4 = "gpt-4"
     GPT35 = "gpt-3.5-turbo"
+    GPT35OLD = "gpt-3.5-turbo-0301"
 
 
 HISTORY_CLEARED = 'HISTORY_CLEARED'
